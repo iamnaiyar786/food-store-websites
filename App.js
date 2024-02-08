0223a2b4d0b79e3417fd7d5a5fad54a6a49e8490
Naiyar Azam <98547126+iamnaiyar786@users.noreply.github.com>
2024-02-08 22:29:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './components/Home';
+import "./assets/css/style.css";
+import Navbar from './components/Navbar';
+import About from './components/About';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    </>
+  );
+}
+
+export default App;
